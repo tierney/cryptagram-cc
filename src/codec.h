690db58fd37dd00bc4db4bb3ea495f563ec1e327
Matt Tierney <tierney@cs.nyu.edu>
2013-04-07 14:16:31 -0400

    Minor codec setup

---
+++ src/codec.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "basictypes.h"
+
+class Codec {
+
+  Codec();
+
+  virtual ~Codec();
+
+
+};
---
+++ src/cryptagram.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include
+
+class Cryptagram {
+ public:
+  explicit Cryptagram(const Codec& codec);
+
+  virtual ~Cryptagram();
+ private:
+
+
+
+};
